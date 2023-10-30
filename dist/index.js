e50066096ff6628b52fe7db8f4374cbc03f5f91f
Nick Liffen <nickliffen@github.com>
2023-10-30 20:42:50 +0000

    moved to node20, removed @

--- dist/index.js
+++ dist/index.js
@@ -6870,7 +6870,7 @@ const filter = async (region, file) => {
         const label = arr.label;
         const override = `${arr.override}`;
         const [singleASE] = arr.approvers;
-        return [approvers, label, override, singleASE.replace("@", '')];
+        return [approvers, label, override, singleASE.replace("@", "")];
     }
     throw new Error(`The region sent from Salesforce did not match a local record. The value from salesforce was: ${region}`);
 };
